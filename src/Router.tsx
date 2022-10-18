7ba153e9549eb4b6c2c4ee116011406a9bdfc130
ricardo <rgranvilla@gmail.com>
2022-10-18 15:32:56 -0300

    feat(github-blog): create index for home and add dependencies to app

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,13 +1,15 @@
 import { Routes, Route } from 'react-router-dom';
 
 import { DefaultLayout } from './layouts/DefaultLayout';
-import { Home } from './pages/Home/Home';
+import { Home } from './pages/Home/';
+import { Post } from './pages/Post';
 
 function Router() {
   return (
     <Routes>
       <Route path="/" element={<DefaultLayout />}>
         <Route path="/" element={<Home />} />
+        <Route path="/Post/:id" element={<Post />} />
       </Route>
     </Routes>
   );
